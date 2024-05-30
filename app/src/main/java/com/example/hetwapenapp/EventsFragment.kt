434e2c4c01eff14493870c365e51ab8617d872ca
DeZeeuwAda <t.temmink@ad-academie.nl>
2024-05-30 20:40:53 +0200

    Evenementenpagina klaar Thema bijgewerkt voor textviews Ronde achtergrond toegevoegd voor uniformiteit

--- app/src/main/java/com/example/hetwapenapp/EventsFragment.kt
+++ app/src/main/java/com/example/hetwapenapp/EventsFragment.kt
@@ -1,5 +1,34 @@
 package com.example.hetwapenapp
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class EventsFragment : Fragment(R.layout.fragment_events) { }
+class EventsFragment : Fragment(R.layout.fragment_events) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val event1Title: TextView = view.findViewById(R.id.titel1)
+        val event1Description: TextView = view.findViewById(R.id.beschrijving1)
+        val event2Title: TextView = view.findViewById(R.id.titel2)
+        val event2Description: TextView = view.findViewById(R.id.beschrijving2)
+
+        event1Title.setOnClickListener {
+            if (event1Description.visibility == View.GONE) {
+                event1Description.visibility = View.VISIBLE
+            } else {
+                event1Description.visibility = View.GONE
+            }
+        }
+
+        event2Title.setOnClickListener {
+            if (event2Description.visibility == View.GONE) {
+                event2Description.visibility = View.VISIBLE
+            } else {
+                event2Description.visibility = View.GONE
+            }
+        }
+    }
+}
